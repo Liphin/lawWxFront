3d2243357735e66dff22bb02b3263c4c43f923d6
capoon <13929580503@163.com>
2019-04-03 16:10:46 +0800

    66

--- project/public/src/content/directive/contentDirective.js
+++ project/public/src/content/directive/contentDirective.js
@@ -176,32 +176,4 @@ contentModule.directive('ngMouseWheel', function () {
     }
 });
 
-contentModule.directive('ngMouseWheel', function () {
-    return {
-        scope: {
-            bgSize: '='
-        },
-        link: function (scope, element, attrs) {
-            element.bind("DOMMouseScroll mousewheel onmousewheel", function (event) {
-                // cross-browser wheel delta
-                var event = window.event || event; // old IE support
-                var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-
-                //对图片进行background-size的操作
-                scope.$apply(function () {
-                    scope.bgSize = scope.bgSize + 10 * delta;
-                });
-
-                //禁止默认的页面滚动操作
-                // for IE
-                event.returnValue = false;
-                // for Chrome and Firefox
-                if (event.preventDefault) {
-                    event.preventDefault();
-                }
-            });
-        }
-    }
-});
-
 
